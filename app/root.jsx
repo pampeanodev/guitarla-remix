33dbdafaa239efae929c0fa9254ebce250d77b0e
Augusto Chirico <pampeanobtc@gmail.com>
2023-02-10 12:17:33 +0100

    add index.css for styles

--- app/root.jsx
+++ app/root.jsx
@@ -1,6 +1,8 @@
 import {
-  Meta
+  Meta,
+  Links
 } from '@remix-run/react'
+import styles from './styles/index.css'
 
 export function meta() {
   return (
@@ -8,6 +10,15 @@ export function meta() {
       charset: 'utf-8',
       title: 'GuitarLA - Remix',
       viewport: 'width=device-width,initial-scale=1'
+    }
+  )
+}
+
+export function links(){
+  return (
+    {
+      rel: 'stylesheet',
+      href: styles
     }
   )
 }
@@ -25,6 +36,7 @@ function Document({children}){
     <html lang="es">
       <head>
         <Meta />
+        <Links />
       </head>
       <body>
         {children}
